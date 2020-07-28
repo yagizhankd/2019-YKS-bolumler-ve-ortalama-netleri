c5a92ab4a9a41229d55974e3b5b080d0e3be6fe7
yagizhankd <yagizhankerim@gmail.com>
2020-07-28 19:14:06 +0300

    yeni dosyalar eklendi
    Verileri çekmek için kullanılan Python dosyası ve daha önceden yine YÖK Atlas üzerinden elde edilen tablo eklendi. SQL dosya ismi düzenlendi.

---
+++ vericekme.py
@@ -0,0 +1,44 @@
+from __future__ import division
+from openpyxl import Workbook,load_workbook
+from bs4 import BeautifulSoup
+import urllib
+from urllib.request import urlopen
+
+wb = load_workbook("sayisalUniVerileri.xlsx")
+ws = wb.active
+query="INSERT INTO netsayilari (bolumID, aytBiyoloji,aytKimya,aytMatematik,tytFen,tytSosyal,tytMatematik,tytTurkce) VALUES"
+for satir in range(1,ws.max_row+1):
+    for sutun in range(1,2):
+        bolumid = str(ws.cell(satir,sutun).value).replace('\t', '')
+    url = "https://yokatlas.yok.gov.tr/content/lisans-dynamic/1210a.php?y="+bolumid
+    sayfa = urllib.request.urlopen(url)
+    soup = BeautifulSoup(sayfa, "html.parser")
+    ana = soup.find('tbody')
+    alt=ana.findAll('td',attrs={"class":"text-center"})
+    durum=str(satir)+" - "+str(ws.max_row)
+    print(durum)
+    word =""
+    counter=0
+    for i in range(len(alt)):
+        if(alt[i].text=="---"):
+            continue
+        if(i<=2):
+            continue
+        if(i%2==0):
+            word+=alt[i].text.replace(',','.')+","
+            counter+=1
+            continue
+    if(counter==8):
+        query+="("
+        query+='"'+bolumid+'"'+","
+        query+=word+"),"
+        print("eklendi")
+    else:
+        print("eklenmedi")
+
+query = query.replace(',)',')')
+query+=";"
+query = query.replace(',;',';')
+dosya = open('bolum-ve-ort-netler.sql', 'w')
+dosya.write(query)
+dosya.close()
