c986ab86f65eb1d3efb20cd1e30951eabfac4c41
yagizhankd <yagizhankerim@gmail.com>
2020-07-28 19:18:12 +0300

    Update 2019-yks.sql

--- 2019-yks.sql
+++ 2019-yks.sql
@@ -19,7 +19,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Veritabanı: `derskonu_wp742`
+-- Veritabanı: `2019-YKS`
 --
 
 -- --------------------------------------------------------
